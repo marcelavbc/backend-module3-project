76e6fb8b1ab20b87da217d14e6ce02cebe257711
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-09-12 15:29:48 +0200

    deploy to heroku

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const User = require('./models/user-model');
 
 
 mongoose
-  .connect('mongodb://localhost/cook-backend', { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
