974aa1ab560486fb7868448363fbf3e87068f1fe
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-09-15 20:08:11 +0200

    deploy 5

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const MongoStore = require('connect-mongo')(session)
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-
 const User = require('./models/user-model');
 // const Recipe = require('./models/recipe-model')
 
@@ -42,7 +41,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 // Express View engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
